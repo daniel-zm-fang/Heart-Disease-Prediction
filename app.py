f6d74ad84bcae6d83d26f1257a5245d13eb0e4fa
Daniel Fang <danielfang2001@gmail.com>
2022-11-08 21:59:01 -0500

    add github logo and link

--- app.py
+++ app.py
@@ -10,11 +10,22 @@ def main():
         page_icon=':heart:',
         layout='wide',
     )
-    col1, col2 = st.columns(spec=[1, 10])
+    col1, col2, col3 = st.columns(spec=[1, 10, 1.4])
     with col1:
         st.image('images/logo.png', width=80)
     with col2:
         st.title('Heart Disease Prediction App')
+    with col3:
+        st.markdown(
+            '''
+            <a href='https://github.com/daniel-zm-fang/Heart-Disease-Prediction' target='_blank'>
+                <img src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' width='80'>
+                <b>Open Source</b>
+            </a>
+            ''',
+            unsafe_allow_html=True
+        )
+
     tab1, tab2, tab3 = st.tabs(['Model Prediction', 'Data Exploration', 'About'])
     with tab1:
         show_prediction_page()
